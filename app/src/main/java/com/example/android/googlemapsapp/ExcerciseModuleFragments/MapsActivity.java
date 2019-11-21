832ff9aae622638b4f3fbec8a65a9a79e291e0c3
Zachary <zlemura29@gmail.com>
2019-11-21 14:05:35 +1100

    Fixed map issue where pindistance would add on top of each other each time a user took the quiz.

--- app/src/main/java/com/example/android/googlemapsapp/ExcerciseModuleFragments/MapsActivity.java
+++ app/src/main/java/com/example/android/googlemapsapp/ExcerciseModuleFragments/MapsActivity.java
@@ -41,6 +41,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        pinStepDistance=0;
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
